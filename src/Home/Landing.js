c17d22c1c901fa1ec15612df5d46964b304e053b
James Sciacca <JamesSciacca1@jamess-mbp.lan>
2023-12-28 19:05:45 -0500

    first push

---
+++ src/Home/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <>
+            <div>
+                <h1 className="">James's Tech Support</h1>
+            </div>
+        </>
+    )
+}
+
+export default Landing;
